25fe392ab176323a04eb9bd70ba1454be4c2a865
H. Cole Wiley <cole@hcwiley.com>
2012-03-12 03:45:05 -0500

    so i renamed everything once again... this is the last time though.

--- practice/tub/apps/developer/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from apps.Developer.models import *
-
-class DeveloperForm(ModelForm):
-    class Meta:
-        model = Developer
--- practice/tub/local_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'HOST': 'localhost',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'codeart72',                      
-        'USER': 'django',
-        'PASSWORD': 'geaux44',                
-    }
-}
