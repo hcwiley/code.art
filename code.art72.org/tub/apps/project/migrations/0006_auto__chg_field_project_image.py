7926138f4309dd0426c7776be701c135cc72664d
H. Cole Wiley <cole@hcwiley.com>
2012-03-14 04:01:44 -0500

    migration 6 for project app

---
+++ code.art72.org/tub/apps/project/migrations/0006_auto__chg_field_project_image.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Renaming column for 'Project.image' to match new field type.
+        db.rename_column('project_project', 'image', 'image_id')
+        # Changing field 'Project.image'
+        db.alter_column('project_project', 'image_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['project.ExtendedImage'], null=True))
+
+        # Adding index on 'Project', fields ['image']
+        db.create_index('project_project', ['image_id'])
+
+
+    def backwards(self, orm):
+        
+        # Removing index on 'Project', fields ['image']
+        db.delete_index('project_project', ['image_id'])
+
+        # Renaming column for 'Project.image' to match new field type.
+        db.rename_column('project_project', 'image_id', 'image')
+        # Changing field 'Project.image'
+        db.alter_column('project_project', 'image', self.gf('sorl.thumbnail.fields.ImageField')(max_length=100, null=True))
+
+
+    models = {
+        'project.extendedimage': {
+            'Meta': {'object_name': 'ExtendedImage'},
+            'external': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'uploaded': ('sorl.thumbnail.fields.ImageField', [], {'default': 'None', 'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'project.media': {
+            'Meta': {'object_name': 'Media'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': "orm['project.ExtendedImage']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '144'}),
+            'video': ('django.db.models.fields.URLField', [], {'default': 'None', 'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'project.project': {
+            'Meta': {'object_name': 'Project'},
+            'blurb': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['project.ExtendedImage']", 'null': 'True', 'blank': 'True'}),
+            'media': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['project.Media']", 'null': 'True', 'blank': 'True'}),
+            'repos': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['project.Repo']", 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'default': "''", 'max_length': '50', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '144'}),
+            'use_git_blurb': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'project.repo': {
+            'Meta': {'object_name': 'Repo'},
+            'blurb': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '144'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['project']
