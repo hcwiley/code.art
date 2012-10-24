cbb061d18620108ef6efd2161503c634c291ebbc
H. Cole Wiley <hcwiley@gmail.com>
2012-10-23 19:57:19 -0500

    updated project.js to have better var names

--- code.art72.org/static/js/project.js
+++ code.art72.org/static/js/project.js
@@ -4,13 +4,14 @@ function initProject() {
 	$('.media-thumbs').each(
 			function(index) {
 				var media = $(this);
+				var mediaY = $(media).attr('date').split('-')[0];
 				var mediaM = $(media).attr('date').split('-')[1];
 				var mediaD = $(media).attr('date').split('-')[2]
 						.substring(0, 2);
 				var dateDiv = $('#' + mediaM + '-' + mediaD);
 				if (dateDiv.length == 0) {
 					dateDiv = $('<div class="dateDiv" id="' + mediaM + '-'
-							+ mediaD + '"><h4>' + mediaM + '-' + mediaD
+							+ mediaD + '"><h4>' + mediaM + '-' + mediaD + '-' + mediaY
 							+ '</h4></div>');
 					$('#timeline').append(dateDiv);
 				}
@@ -18,18 +19,20 @@ function initProject() {
 			});
 	$('.commit').each(
 			function(index) {
-				var commitM = $(this).attr('date').split('-')[1];
-				var commitD = $(this).attr('date').split('-')[2]
+				var commit = $(this);
+				var commitY = $(commit).attr('date').split('-')[0];
+				var commitM = $(commit).attr('date').split('-')[1];
+				var commitD = $(commit).attr('date').split('-')[2]
 						.substring(0, 2);
-				var commitTime = $(this).attr('date').split('-')[2].substring(3);
-				$($(this).children('p')[0]).text(commitTime+': ');
+				var commitTime = $(commit).attr('date').split('-')[2].substring(3);
+				$($(commit).children('p')[0]).text(commitTime+': ');
 				var dateDiv = $('#' + commitM + '-' + commitD);
 				if (dateDiv.length == 0) {
 					dateDiv = $('<div class="dateDiv" id="' + commitM + '-'
-							+ commitD + '"><h4>' + commitM + '-' + commitD
+							+ commitD + '"><h4>' + commitM + '-' + commitD + '-' + commitY
 							+ '</h4></div>');
 					$('#timeline').append(dateDiv);
 				}
-				$(this).appendTo($(dateDiv));
+				$(commit).appendTo($(dateDiv));
 			});
 }
